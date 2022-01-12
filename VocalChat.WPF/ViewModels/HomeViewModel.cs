9cef9e84399dab268f469a4e6f0b234f58bb8fed
Aspirio <soultan_98@hotmail.com>
2022-01-12 17:31:18 +0100

    feat: Load list of input/output and selected too

--- VocalChat.WPF/ViewModels/HomeViewModel.cs
+++ VocalChat.WPF/ViewModels/HomeViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +15,74 @@ namespace VocalChat.WPF.ViewModels
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public void OnPropertyChanged(string propertyName)
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<MMDevice> AvailableOutputs { get; set; }
+        public ObservableCollection<MMDevice> AvailableInputs { get; set; }
+
+        private MMDevice _selectedOutput;
+
+        public MMDevice SelectedOutput
+        {
+            get { return _selectedOutput; }
+            set
+            {
+                _selectedOutput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private MMDevice _selectedInput;
+
+        public MMDevice SelectedInput
+        {
+            get { return _selectedInput; }
+            set
+            {
+                _selectedInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public HomeViewModel()
+        {
+            AvailableOutputs = LoadOutputList();
+            AvailableInputs = LoadInputList();
+        }
+
+        public ObservableCollection<MMDevice> LoadOutputList()
+        {
+            ObservableCollection<MMDevice> outputs = new ObservableCollection<MMDevice>();
+
+            using (MMDeviceEnumerator? enumarator = new MMDeviceEnumerator())
+            {
+                foreach (MMDevice? output in enumarator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active))
+                {
+                    outputs.Add(output);
+                }
+            }
+
+            return outputs;
+        }
+
+        public ObservableCollection<MMDevice> LoadInputList()
+        {
+            ObservableCollection<MMDevice> inputs = new ObservableCollection<MMDevice>();
+
+            using (MMDeviceEnumerator? enumerator = new MMDeviceEnumerator())
+            {
+                foreach (MMDevice? input in enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active))
+                {
+                    inputs.Add(input);
+                }
+            }
+
+            return inputs;
         }
     }
 }
