a64fb960e46b6f5c195a47eb3eb1d4c30cd89a85
Aspirio <soultan_98@hotmail.com>
2022-01-12 16:45:18 +0100

    feat: Created the 3 needed projects: WPF, Web API and shared class library

---
+++ VocalChat.WPF/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using VocalChat.WPF.Views;
+
+namespace VocalChat.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<HomeView>();
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            HomeView? homeView = _serviceProvider.GetService<HomeView>();
+            if (homeView is null) throw new Exception("Application cannot start!");
+            homeView.Show();
+        }
+    }
+}
